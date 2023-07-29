57d0f6c09aa143cba533d946812abe2b09e26211
threeSeven <nuomibaicha@163.com>
2023-07-29 21:42:04 +0800

    update 精简

--- src/test/java/com/yidiansishiyi/aimodule/service/UserServiceTest.java
+++ src/test/java/com/yidiansishiyi/aimodule/service/UserServiceTest.java
@@ -20,7 +20,7 @@ public class UserServiceTest {
 
     @Test
     void userRegister() {
-        String userAccount = "yupi";
+        String userAccount = "yidiansishiyi";
         String userPassword = "";
         String checkPassword = "123456";
         try {
